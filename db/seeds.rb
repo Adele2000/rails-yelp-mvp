2c3231b876afd2059b6ee102dc105e725b710993
Adele Albrespy <a.albrespy@gmail.com>
2023-02-22 09:02:53 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,5 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-require 'faker'
-
-Restaurant.destroy_all
-
-category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
-
-5.times do
-  restaurant = Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.city, category: category.sample)
-  10.times do
-    review = Review.new(content: Faker::Restaurant.review, rating: Faker::Number.between(from: 1, to: 5))
-    review.restaurant = restaurant
-    review.save!
-  end
-end
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
